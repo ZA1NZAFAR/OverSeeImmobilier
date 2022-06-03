3ebe4aedc1854a98e85d489b0260c7581ed4dfa2
Zain ZAFAR <zain.zafar@sap.com>
2022-06-04 01:16:28 +0200

    refactoring Property

--- src/main/java/servlets/ManagePropertyServlet.java
+++ src/main/java/servlets/ManagePropertyServlet.java
@@ -3,6 +3,7 @@ package servlets;
 import lombok.SneakyThrows;
 import models.Log;
 import tools.DatabaseConnector;
+import tools.HtmlDisplayer;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +17,36 @@ public class ManagePropertyServlet extends HttpServlet {
     @SneakyThrows
     public void doPost(HttpServletRequest req, HttpServletResponse resp) {
         String action = req.getParameter("action");
+
+        // Import des données
+        String adr = req.getParameter("tf_adr");
+        String vll = req.getParameter("tf_ville");
+        String cp = req.getParameter("tf_cp");
+        String type = req.getParameter("listT_biens");
+        String piece = req.getParameter("stepper_nbPiece");
+        String sprf = req.getParameter("tf_superficie");
+        String etat = req.getParameter("list_type");
+        String garage = req.getParameter("stepper_garage");
+        String prx = req.getParameter("stepper_prix");
+        String dte = req.getParameter("date");
+        String idPr = req.getParameter("list_prop");
+        String locVe = req.getParameter("tf_locVent");
+
+        // Les checks
+        if (action.equals("add") && (adr == null || vll == null || cp == null || type == null || piece == null || sprf == null || etat == null || garage == null || prx == null || dte == null || locVe == null)) {
+            HtmlDisplayer.warning(req, resp, "Veuillez remplir tous les champs!");
+        } else if (cp.length() != 5) {
+            HtmlDisplayer.warning(req, resp, "Le code postal doit être composé de 5 chiffres!");
+        } else if (Integer.parseInt(piece) < 1)
+            HtmlDisplayer.warning(req, resp, "Le nombre de pièces doit être supérieur à 0!");
+        else if (Integer.parseInt(sprf) < 1)
+            HtmlDisplayer.warning(req, resp, "La superficie doit être supérieur à 0!");
+        else if (Integer.parseInt(garage) < 0)
+            HtmlDisplayer.warning(req, resp, "Le nombre de garage doit être supérieur ou égal à 0!");
+        else if (Integer.parseInt(prx) < 0)
+            HtmlDisplayer.warning(req, resp, "Le prix doit être supérieur ou égal à 0!");
+
+
         String propertyId = req.getParameter("propertyId");
         switch (action) {
             case "edit":
@@ -27,20 +58,21 @@ public class ManagePropertyServlet extends HttpServlet {
                 resp.sendRedirect("views/biens/gestion.jsp");
                 break;
             case "update":
-                DatabaseConnector.executeUpdate("UPDATE Propriete SET adresse = '" +
-                        req.getParameter("tf_adr") + "', ville = '" +
-                        req.getParameter("tf_ville") + "', codePostal = " +
-                        req.getParameter("tf_cp") + ", type = '" +
-                        req.getParameter("listT_biens") + "', nombre_De_Piece = " +
-                        req.getParameter("stepper_nbPiece") + ", superficie = " +
-                        req.getParameter("tf_superficie") + ", etat_d_habitation = '" +
-                        req.getParameter("list_type") + "', garage = " +
-                        req.getParameter("stepper_garage") + ", prixInitial = " +
-                        req.getParameter("stepper_prix") + ", dateDisponibilite = '" +
-                        req.getParameter("date") + "', idProprietaire = " +
-                        req.getParameter("list_prop") + ", locationOuVente = '" +
-                        req.getParameter("tf_locVent") + "' WHERE numeroReference = " +
-                        req.getParameter("refBien") + ";");
+                DatabaseConnector.executeUpdate("UPDATE Propriete " +
+                        "SET " +
+                        "adresse = '" + adr + "', " +
+                        "ville = '" + vll + "', " +
+                        "codePostal = " + cp + ", " +
+                        "type = '" + type + "', " +
+                        "nombre_De_Piece = " + piece + ", " +
+                        "superficie = " + sprf + ", " +
+                        "etat_d_habitation = '" + etat + "', " +
+                        "garage = " + garage + ", " +
+                        "prixInitial = " + prx + ", " +
+                        "dateDisponibilite = '" + dte + "', " +
+                        "idProprietaire = " + idPr + ", " +
+                        "locationOuVente = '" + locVe + "' " +
+                        "WHERE numeroReference = " + req.getParameter("refBien") + ";");
                 DatabaseConnector.log(Log.builder().idAgent((Long) req.getSession().getAttribute("idAgent")).action("Modification").information("Propriété " + propertyId).build());
                 resp.sendRedirect("views/biens/gestion.jsp");
                 break;
@@ -60,18 +92,18 @@ public class ManagePropertyServlet extends HttpServlet {
                         "dateDisponibilite, " +
                         "idProprietaire) VALUES ('" +
                         (DatabaseConnector.getLastId("Propriete", "numeroReference") + 1) + "', '" +
-                        req.getParameter("tf_adr") + "', '" +
-                        req.getParameter("tf_ville") + "', " +
-                        req.getParameter("tf_cp") + ", '" +
-                        req.getParameter("listT_biens") + "', " +
-                        req.getParameter("stepper_nbPiece") + ", " +
-                        req.getParameter("tf_superficie") + ", '" +
-                        req.getParameter("list_type") + "', " +
-                        req.getParameter("stepper_garage") + ", " +
-                        req.getParameter("stepper_prix") + ", '" +
-                        req.getParameter("tf_locVent") + "', '" +
-                        req.getParameter("date") + "', " +
-                        req.getParameter("list_prop") + ");");
+                        adr + "', '" +
+                        vll + "', " +
+                        cp + ", '" +
+                        type + "', " +
+                        piece + ", " +
+                        sprf + ", '" +
+                        etat + "', " +
+                        garage + ", " +
+                        prx + ", '" +
+                        locVe + "', '" +
+                        dte + "', " +
+                        idPr + ");");
                 DatabaseConnector.log(Log.builder().idAgent((Long) req.getSession().getAttribute("idAgent")).action("Ajout").information("Propriété " + propertyId).build());
                 resp.sendRedirect("views/biens/gestion.jsp");
                 break;
