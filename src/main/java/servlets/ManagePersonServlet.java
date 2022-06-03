78df80ee03ad8d863dad9fbfe70933e71fbc9506
Zain ZAFAR <zain.zafar@sap.com>
2022-06-04 01:02:44 +0200

    refactoring Person

--- src/main/java/servlets/ManagePersonServlet.java
+++ src/main/java/servlets/ManagePersonServlet.java
@@ -15,7 +15,36 @@ public class ManagePersonServlet extends HttpServlet {
 
     @SneakyThrows
     public void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        // Information basique
         String action = req.getParameter("action");
+        String nom = req.getParameter("tf_nom");
+        String prnm = req.getParameter("tf_prenom");
+        String ddn = req.getParameter("dateNaissance");
+        String addr = req.getParameter("tf_adr");
+        String vll = req.getParameter("tf_ville");
+        String cp = req.getParameter("tf_cp");
+        String tel = req.getParameter("tf_tel");
+        String mel = req.getParameter("tf_email");
+
+        // Information agent immobilier
+        String mdp = req.getParameter("mdp");
+        String slr = req.getParameter("salaire");
+        String demb = req.getParameter("dateEmb");
+
+        // Les checks
+        if ((action.equals("add") || action.equals("update")) && (nom == null || prnm == null || ddn == null || addr == null || vll == null || cp == null || tel == null || mel == null)) {
+            HtmlDisplayer.warning(req, resp, "Veuillez remplir tous les champs!");
+        } else if (cp.length() != 5) {
+            HtmlDisplayer.warning(req, resp, "Le code postal doit être composé de 5 chiffres!");
+        } else if (tel.length() != 10) {
+            HtmlDisplayer.warning(req, resp, "Le numéro de téléphone doit être composé de 10 chiffres!");
+        } else if (mel.length() != 0 && !mel.contains("@")) {
+            HtmlDisplayer.warning(req, resp, "L'adresse email doit être valide!");
+        } else if (action.equals("add") && (mdp == null || slr == null || demb == null)) {
+            HtmlDisplayer.warning(req, resp, "Veuillez remplir tous les champs de l'agent immobilier!");
+        }
+
+        // Identification de ka personne (si elle existe)
         String clientId = req.getParameter("clientId");
         String proprietaireId = req.getParameter("propId");
         String personneId = req.getParameter("idPersonne");
@@ -28,10 +57,10 @@ public class ManagePersonServlet extends HttpServlet {
             personId = DatabaseConnector.getPersonneById((int) DatabaseConnector.getProprietaireById(Integer.parseInt(proprietaireId)).getIdPersonne()).getIdPersonne();
         } else if (personneId != null) {
             personId = DatabaseConnector.getPersonneById(Integer.parseInt(personneId)).getIdPersonne();
-        } else if (idAgent!= null){
+        } else if (idAgent != null) {
             personId = DatabaseConnector.getPersonneById((int) DatabaseConnector.getAgentById(Integer.parseInt(idAgent)).getIdPersonne()).getIdPersonne();
-        }else if (!action.equals("add")) {
-            HtmlDisplayer.error(req, resp, "No person selected");
+        } else if (!action.equals("add")) {
+            HtmlDisplayer.error(req, resp, "No person selected!");
         }
 
         switch (action) {
@@ -45,14 +74,14 @@ public class ManagePersonServlet extends HttpServlet {
                 break;
             case "update":
                 DatabaseConnector.executeUpdate("UPDATE Personne SET " +
-                        "nom = '" + req.getParameter("tf_nom") + "', " +
-                        "prenom = '" + req.getParameter("tf_prenom") + "', " +
-                        "dateNaissance = '" + req.getParameter("dateNaissance") + "', " +
-                        "adresse = '" + req.getParameter("tf_adr") + "', " +
-                        "ville = '" + req.getParameter("tf_ville") + "', " +
-                        "codePostal = '" + req.getParameter("tf_cp") + "', " +
-                        "numeroTel = '" + req.getParameter("tf_tel") + "', " +
-                        "email = '" + req.getParameter("tf_email") + "' " +
+                        "nom = '" + nom + "', " +
+                        "prenom = '" + prnm + "', " +
+                        "dateNaissance = '" + ddn + "', " +
+                        "adresse = '" + addr + "', " +
+                        "ville = '" + vll + "', " +
+                        "codePostal = '" + cp + "', " +
+                        "numeroTel = '" + tel + "', " +
+                        "email = '" + mel + "' " +
                         "WHERE idPersonne = " + personId);
                 DatabaseConnector.log(Log.builder().idAgent((Long) req.getSession().getAttribute("idAgent")).action("Update").information("Personne " + personId).build());
                 resp.sendRedirect("views/clients/gestion.jsp");
@@ -70,14 +99,14 @@ public class ManagePersonServlet extends HttpServlet {
                         "dateNaissance)" +
                         "VALUES (" +
                         (DatabaseConnector.getLastId("Personne", "idPersonne") + 1) + ", '" +
-                        req.getParameter("tf_nom") + "', '" +
-                        req.getParameter("tf_prenom") + "','" +
-                        req.getParameter("tf_tel") + "','" +
-                        req.getParameter("tf_email") + "','" +
-                        req.getParameter("tf_adr") + "','" +
-                        req.getParameter("tf_ville") + "','" +
-                        req.getParameter("tf_cp") + "','" +
-                        req.getParameter("dateNaissance") + "')");
+                        nom + "', '" +
+                        prnm + "','" +
+                        tel + "','" +
+                        mel + "','" +
+                        addr + "','" +
+                        vll + "','" +
+                        cp + "','" +
+                        ddn + "')");
 
                 DatabaseConnector.log(Log.builder().idAgent((Long) req.getSession().getAttribute("idAgent")).action("Ajout").information("Client " + (DatabaseConnector.getLastId("Personne", "idPersonne"))).build());
 
@@ -113,10 +142,10 @@ public class ManagePersonServlet extends HttpServlet {
                             "VALUES ('" +
                             (DatabaseConnector.getLastId("AgentImmobilier", "idAgentImmobilier") + 1) + "','" +
                             (DatabaseConnector.getLastId("Personne", "idPersonne")) + "','" +
-                            req.getParameter("mdp") + "','" +
+                            mdp + "','" +
                             ((req.getParameter("estAdmin") != null) ? "oui" : "non") + "','" +
-                            req.getParameter("salaire") + "','" +
-                            req.getParameter("dateEmb") + "')");
+                            slr + "','" +
+                            demb + "')");
                     DatabaseConnector.log(Log.builder().idAgent((Long) req.getSession().getAttribute("idAgent")).action("Ajout").information("Agent " + (DatabaseConnector.getLastId("AgentImmobilier", "idAgentImmobilier"))).build());
                 }
 
